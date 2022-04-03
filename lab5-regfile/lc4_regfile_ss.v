5b8f3f872cdf723fb6d40c8e0af1ff5557e7815e
Zeyu Jason Niu <jasonzy@huntsman-ve703-00787.apn.wlan.private.upenn.edu>
2022-04-03 18:24:21 -0400

    lab50reg

--- lab5-regfile/lc4_regfile_ss.v
+++ lab5-regfile/lc4_regfile_ss.v
@@ -89,7 +89,7 @@ module lc4_regfile_ss #(parameter n = 16)
     assign we6 = ( ((i_rd_A == 3'd6) & we_A) || ((i_rd_B == 3'd6) & i_rd_we_B) ) ? 1'b1 : 1'b0;
     assign we7 = ( ((i_rd_A == 3'd7) & we_A) || ((i_rd_B == 3'd7) & i_rd_we_B) ) ? 1'b1 : 1'b0;
 
-    
+    // Registers to hold values
     Nbit_reg #(n) r0 (.in(i_data_r0), .out(rv0), .clk(clk), .we(we0), .gwe(gwe), .rst(rst));
     Nbit_reg #(n) r1 (.in(i_data_r1), .out(rv1), .clk(clk), .we(we1), .gwe(gwe), .rst(rst));
     Nbit_reg #(n) r2 (.in(i_data_r2), .out(rv2), .clk(clk), .we(we2), .gwe(gwe), .rst(rst));
@@ -99,6 +99,7 @@ module lc4_regfile_ss #(parameter n = 16)
     Nbit_reg #(n) r6 (.in(i_data_r6), .out(rv6), .clk(clk), .we(we6), .gwe(gwe), .rst(rst));
     Nbit_reg #(n) r7 (.in(i_data_r7), .out(rv7), .clk(clk), .we(we7), .gwe(gwe), .rst(rst));
 
+    // MUX for output value selection
     Mux8to1 mux_rs_A(.sel(i_rs_A), .rv0(rv0), .rv1(rv1), .rv2(rv2), .rv3(rv3), 
     .rv4(rv4), .rv5(rv5), .rv6(rv6), .rv7(rv7), .o_data(o_rs_A));
     Mux8to1 mux_rt_A(.sel(i_rt_A), .rv0(rv0), .rv1(rv1), .rv2(rv2), .rv3(rv3), 
@@ -110,10 +111,9 @@ module lc4_regfile_ss #(parameter n = 16)
 
     //Bypass write-in value to output
     assign o_rs_data_A =    ((i_rs_A == i_rd_A) & i_rd_we_A) ? i_wdata_A : 
-                            ((i_rs_A == i_rd_B) & i_rd_we_B) ? i_wdata_B :
-                            ((i_rs_A == i_rd_A) & (we_A==0)) ?  : o_rs_A;
-
-    assign o_rt_data_A =    (((i_rt_A == i_rd_A) & i_rd_we_A)) ? i_wdata_A : 
+                            ((i_rs_A == i_rd_B) & i_rd_we_B) ? i_wdata_B : o_rs_A;
+                            
+    assign o_rt_data_A =    ((i_rt_A == i_rd_A) & i_rd_we_A) ? i_wdata_A : 
                             ((i_rt_A == i_rd_B) & i_rd_we_B) ? i_wdata_B : o_rt_A;
 
     assign o_rs_data_B =    ((i_rs_B == i_rd_B) & i_rd_we_B) ? i_wdata_B : 
