6a8e423ddf89cb84ccfb71730eb92a7a32260874
Zeyu Jason Niu <jasonzy@huntsman-ve703-00787.apn.wlan.private.upenn.edu>
2022-04-04 14:04:18 -0400

    lab5-copy-complete

--- lab5-superscalar/lc4_superscalar.v
+++ lab5-superscalar/lc4_superscalar.v
@@ -51,28 +51,51 @@ module lc4_processor(input wire         clk,             // main clock
 
     // PC fetch and plus one before D stage //
     wire    [15:0] f2d_pc_plus_one;
-    cla16 Pipeline_PC_Inc(.a(f2d_pc), .b(16'b0), .cin(1'b1), .sum(f2d_pc_plus_one));
+    cla16 Pipeline_PC_Inc(.a(f2d_pc), .b(16'd2), .cin(1'b0), .sum(f2d_pc_plus_one));
 
     // Register file for Pipelned Datapath //
-    wire [15:0] o_regfile_rs, o_regfile_rt;              
-    lc4_regfile Pipeline_Regfile (
+    wire [15:0] o_regfile_rs_A, o_regfile_rt_A,
+                o_regfile_rs_B, o_regfile_rt_B;              
+    lc4_regfile_ss Superscaler_Regfile (
             .clk(clk),
             .gwe(gwe),
             .rst(rst),
-            .i_rs(d2x_bus[33:31]), 
-            .o_rs_data(o_regfile_rs),
-            .i_rt(d2x_bus[30:28]), 
-            .o_rt_data(o_regfile_rt),
-            .i_rd(w_o_bus[27:25]), 
-            .i_wdata(write_back), 
-            .i_rd_we(w_o_bus[22])
+
+            .i_rs_A(d2x_bus_A[33:31]),
+            .i_rs_B(d2x_bus_B[33:31]) 
+            .o_rs_data_A(o_regfile_rs_A),
+            .o_rs_data_B(o_regfile_rs_B),
+
+            .i_rt_A(d2x_bus_A[30:28]),
+            .i_rt_B(d2x_bus_B[30:28]), 
+            .o_rt_data_A(o_regfile_rt_A),
+            .o_rt_data_B(o_regfile_rt_B),
+
+            .i_rd_A(w_o_bus_A[27:25]), 
+            .i_rd_B(w_o_bus_B[27:25]),            
+            .i_rd_we_A(w_o_bus_A[22]),
+            .i_rd_we_B(w_o_bus_B[22]),
+            .i_wdata_A(write_back_A),
+            .i_wdata_B(write_back_B),
     );
-        
+    
+    // lc4_regfile Pipeline_Regfile (
+    //         .clk(clk),
+    //         .gwe(gwe),
+    //         .rst(rst),
+    //         .i_rs(d2x_bus[33:31]), 
+    //         .o_rs_data(o_regfile_rs),
+    //         .i_rt(d2x_bus[30:28]), 
+    //         .o_rt_data(o_regfile_rt),
+    //         .i_rd(w_o_bus[27:25]), 
+    //         .i_wdata(write_back), 
+    //         .i_rd_we(w_o_bus[22])
+    // );
 
     /**** Registers for Intermediate States ****/
 
     // Intermediate PC registers //
-    wire [15:0]     next_pc;
+    wire [15:0]     next_pc_A, next_pc_;
     wire [15:0]     f2d_pc, d2x_pc, x2m_pc, m2w_pc, w_o_pc; 
 
     Nbit_reg #(16, 16'h8200) f_pc_reg (.in(next_pc), .out(f2d_pc), .clk(clk), .we(~load2use), .gwe(gwe), .rst(rst));
@@ -83,19 +106,29 @@ module lc4_processor(input wire         clk,             // main clock
 
 
     // Instructions registers //
-    wire            x_br_taken_or_ctrl, branch_taken;                   // Taking branch or control instruction will flush the current cycle
-    wire            load2use;                                           // Judge whether there is a load-to-use stall
-    wire [15:0]     d_i_bus_a, d2x_bus_tmp;                               // PC bus at D and pre_X stage
-    wire [33:0]     d2x_bus_a, d2x_bus_final_a, x2m_bus_a, m2w_bus_a, w_o_bus_a;  // Intermediate buses 
+    wire            x_br_taken_or_ctrl_A, branch_taken_A,
+                    x_br_taken_or_ctrl_B, branch_taken_B,;                              // Taking branch or control instruction will flush the current cycle
+    wire            load2use_A, load2use_A;                                             // Judge whether there is a load-to-use stall
+    wire [15:0]     d_i_bus_A, d2x_bus_tmp_A, d_i_bus_B, d2x_bus_tmp_B;                 // PC bus at D and pre_X stage
+    wire [33:0]     d2x_bus_A, d2x_bus_final_A, x2m_bus_A, m2w_bus_A, w_o_bus_A,
+                    d2x_bus_B, d2x_bus_final_B, x2m_bus_B, m2w_bus_B, w_o_bus_B;        // Intermediate buses 
 
-    Nbit_reg #(16, 16'b0) d_insn_reg_a (.in(d_i_bus_a), .out(d2x_bus_tmp_a), .clk(clk), .we(~load2use), .gwe(gwe), .rst(rst));
-    Nbit_reg #(34, 34'b0) x_insn_reg_a (.in(d2x_bus_final_a), .out(x2m_bus_a), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
-    Nbit_reg #(34, 34'b0) m_insn_reg_a (.in(x2m_bus_a), .out(m2w_bus_a), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
-    Nbit_reg #(34, 34'b0) w_insn_reg_a (.in(m2w_bus_a), .out(w_o_bus_a), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(16, 16'b0) d_insn_reg_A (.in(d_i_bus_A), .out(d2x_bus_tmp_A), .clk(clk), .we(~load2use_A), .gwe(gwe), .rst(rst));
+    Nbit_reg #(34, 34'b0) x_insn_reg_A (.in(d2x_bus_final_A), .out(x2m_bus_A), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(34, 34'b0) m_insn_reg_A (.in(x2m_bus_A), .out(m2w_bus_A), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(34, 34'b0) w_insn_reg_A (.in(m2w_bus_A), .out(w_o_bus_A), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
 
-    assign  d_i_bus = (x_br_taken_or_ctrl == 1) ? {16{1'b0}} : i_cur_insn;                  // misprediction and control signal causes flush
-    assign  d2x_bus[15:0] = d2x_bus_tmp;
-    assign  d2x_bus_final = ((load2use | x_br_taken_or_ctrl) == 1) ? {34{1'b0}} : d2x_bus;  // load2use also causes flush, but only judged after D stage//
+    Nbit_reg #(16, 16'b0) d_insn_reg_B (.in(d_i_bus_B), .out(d2x_bus_tmp_B), .clk(clk), .we(~load2use_B), .gwe(gwe), .rst(rst));
+    Nbit_reg #(34, 34'b0) x_insn_reg_B (.in(d2x_bus_final_B), .out(x2m_bus_B), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(34, 34'b0) m_insn_reg_B (.in(x2m_bus_B), .out(m2w_bus_B), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(34, 34'b0) w_insn_reg_B (.in(m2w_bus_B), .out(w_o_bus_B), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+
+    assign  d_i_bus_A = (x_br_taken_or_ctrl_A == 1) ? {16{1'b0}} : i_cur_insn_A;
+    assign  d_i_bus_A = (x_br_taken_or_ctrl_B == 1) ? {16{1'b0}} : i_cur_insn_B;                        // misprediction and control signal causes flush
+    assign  d2x_bus_A[15:0] = d2x_bus_tmp_A;
+    assign  d2x_bus_B[15:0] = d2x_bus_tmp_B;
+    assign  d2x_bus_final_A = ((load2use_A | x_br_taken_or_ctrl_A) == 1) ? {34{1'b0}} : d2x_bus_A;
+    assign  d2x_bus_final_B = ((load2use_B | x_br_taken_or_ctrl_B) == 1) ? {34{1'b0}} : d2x_bus_B;      // load2use also causes flush, but only judged after D stage//
 
 
     // Wires to calculating br_predict and next PC //
@@ -109,22 +142,36 @@ module lc4_processor(input wire         clk,             // main clock
 
 
     // Regiters for Intermediate A, B, O, D Input/Output //
-    wire [15:0]     x_A_i, x_A_o, x_B_i, x_B_o,
-                    m_B_o, m_O_i, m_O_o, 
-                    w_O_o, w_D_i, w_D_o;
-    wire [15:0]     write_back;                  // Determine the value that writes back to the Regfiles //
+    wire [15:0]     x_A_i_A, x_A_o_A, x_B_i_A, x_B_o_A, m_B_o_A, 
+                    m_O_i_A, m_O_o_A, w_O_o_A, w_D_i_A, w_D_o_A,
+                    x_A_i_B, x_A_o_B, x_B_i_B, x_B_o_B, m_B_o_B, 
+                    m_O_i_B, m_O_o_B, w_O_o_B, w_D_i_B, w_D_o_B;
+    wire [15:0]     write_back_A, write_back_B;                  // Determine the value that writes back to the Regfiles //
                     
-    Nbit_reg #(16, 16'b0) x_A_reg (.in(x_A_i), .out(x_A_o), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
-    Nbit_reg #(16, 16'b0) x_B_reg (.in(x_B_i), .out(x_B_o), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
-    Nbit_reg #(16, 16'b0) m_B_reg (.in(rt_bypass_res), .out(m_B_o), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
-    Nbit_reg #(16, 16'b0) m_O_reg (.in(m_O_i), .out(m_O_o), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
-    Nbit_reg #(16, 16'b0) w_O_reg (.in(m_O_o), .out(w_O_o), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
-    Nbit_reg #(16, 16'b0) w_D_reg (.in(i_cur_dmem_data), .out(w_D_o), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(16, 16'b0) x_A_reg_A (.in(x_A_i_A), .out(x_A_o_A), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(16, 16'b0) x_B_reg_A (.in(x_B_i_A), .out(x_B_o_A), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(16, 16'b0) m_B_reg_A (.in(rt_bypass_res_A), .out(m_B_o_A), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(16, 16'b0) m_O_reg_A (.in(m_O_i_A), .out(m_O_o_A), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(16, 16'b0) w_O_reg_A (.in(m_O_o_A), .out(w_O_o_A), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(16, 16'b0) w_D_reg_A (.in(i_cur_dmem_data_A), .out(w_D_o_A), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
     
-    assign  x_A_i = ((w_o_bus[27:25] == d2x_bus[33:31]) && w_o_bus[22]) ? write_back : o_regfile_rs; 
-    assign  x_B_i = ((w_o_bus[27:25] == d2x_bus[30:28]) && w_o_bus[22]) ? write_back : o_regfile_rt;
-    assign  m_O_i = (x2m_bus[16] == 1) ? d2x_pc : o_alu_result;                     
-    assign  write_back = (w_o_bus[19] == 1) ? w_D_o : w_O_o;        // Write back to register
+    Nbit_reg #(16, 16'b0) x_A_reg_B (.in(x_A_i_B), .out(x_A_o_B), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(16, 16'b0) x_B_reg_B (.in(x_B_i_B), .out(x_B_o_B), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(16, 16'b0) m_B_reg_B (.in(rt_bypass_res_B), .out(m_B_o_B), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(16, 16'b0) m_O_reg_B (.in(m_O_i_B), .out(m_O_o_B), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(16, 16'b0) w_O_reg_B (.in(m_O_o_B), .out(w_O_o_B), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    Nbit_reg #(16, 16'b0) w_D_reg_B (.in(i_cur_dmem_data_B), .out(w_D_o_A), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+    
+    assign  x_A_i_A = ((w_o_bus_A[27:25] == d2x_bus_A[33:31]) && w_o_bus_A[22]) ? write_back_A : o_regfile_rs_A; 
+    assign  x_B_i_A = ((w_o_bus_A[27:25] == d2x_bus_A[30:28]) && w_o_bus_A[22]) ? write_back_A : o_regfile_rt_A;   
+    assign  x_A_i_B = ((w_o_bus_B[27:25] == d2x_bus_B[33:31]) && w_o_bus_B[22]) ? write_back_B : o_regfile_rs_B; 
+    assign  x_B_i_B = ((w_o_bus_B[27:25] == d2x_bus_B[30:28]) && w_o_bus_B[22]) ? write_back_B : o_regfile_rt_B;
+
+    assign  m_O_i_A = (x2m_bus_A[16] == 1) ? d2x_pc_A : o_alu_result_A;
+    assign  m_O_i_B = (x2m_bus_B[16] == 1) ? d2x_pc_B : o_alu_result_B; 
+
+    assign  write_back_A = (w_o_bus_A[19] == 1) ? w_D_o : w_O_o;
+    assign  write_back_B = (w_o_bus_B[19] == 1) ? w_D_o : w_O_o                 // Write back to register
 
 
     // Registers for stall cycle //
@@ -192,47 +239,57 @@ module lc4_processor(input wire         clk,             // main clock
 
     /***** Pipeline Stage2: Decoder *****/
     lc4_decoder PipeA_Decoder (
-            .r1sel(d2x_bus_a[33:31]), 
-            .r2sel(d2x_bus_a[30:28]),
-            .wsel(d2x_bus_a[27:25]),
-            .r1re(d2x_bus_a[24]),
-            .r2re(d2x_bus_a[23]),
-            .regfile_we(d2x_bus_a[22]),
-            .nzp_we(d2x_bus_a[21]), 
-            .select_pc_plus_one(d2x_bus_a[20]),
-            .is_load(d2x_bus_a[19]), 
-            .is_store(d2x_bus_a[18]),
-            .is_branch(d2x_bus_a[17]), 
-            .is_control_insn(d2x_bus_a[16]),
-            .insn(d2x_bus_a[15:0])
+            .r1sel(d2x_bus_A[33:31]), 
+            .r2sel(d2x_bus_A[30:28]),
+            .wsel(d2x_bus_A[27:25]),
+            .r1re(d2x_bus_A[24]),
+            .r2re(d2x_bus_A[23]),
+            .regfile_we(d2x_bus_A[22]),
+            .nzp_we(d2x_bus_A[21]), 
+            .select_pc_plus_one(d2x_bus_A[20]),
+            .is_load(d2x_bus_A[19]), 
+            .is_store(d2x_bus_A[18]),
+            .is_branch(d2x_bus_a_A[17]), 
+            .is_control_insn(d2x_bus_a_A[16]),
+            .insn(d2x_bus_a_A[15:0])
     );
     
     lc4_decoder PipeB_Decoder (
-            .r1sel(d2x_bus_b[33:31]), 
-            .r2sel(d2x_bus_b[30:28]),
-            .wsel(d2x_bus_b[27:25]),
-            .r1re(d2x_bus_b[24]),
-            .r2re(d2x_bus_b[23]),
-            .regfile_we(d2x_bus_b[22]),
-            .nzp_we(d2x_bus_b[21]), 
-            .select_pc_plus_one(d2x_bus_b[20]),
-            .is_load(d2x_bus_b[19]), 
-            .is_store(d2x_bus_b[18]),
-            .is_branch(d2x_bus_b[17]), 
-            .is_control_insn(d2x_bus_b[16]),
-            .insn(d2x_bus_b[15:0])
+            .r1sel(d2x_bus_B[33:31]), 
+            .r2sel(d2x_bus_B[30:28]),
+            .wsel(d2x_bus_B[27:25]),
+            .r1re(d2x_bus_B[24]),
+            .r2re(d2x_bus_B[23]),
+            .regfile_we(d2x_bus_B[22]),
+            .nzp_we(d2x_bus_B[21]), 
+            .select_pc_plus_one(d2x_bus_B[20]),
+            .is_load(d2x_bus_B[19]), 
+            .is_store(d2x_bus_B[18]),
+            .is_branch(d2x_bus_B[17]), 
+            .is_control_insn(d2x_bus_B[16]),
+            .insn(d2x_bus_B[15:0])
     );
     
     /***** Pipeline Stage3: Execute (ALU) *****/
-    wire    [15:0] o_alu_result, rs_bypass_res, rt_bypass_res, wm_bypass_res;
+    wire    [15:0]  o_alu_result_A, rs_bypass_res_A, rt_bypass_res_A, wm_bypass_res_A,
+                    o_alu_result_B, rs_bypass_res_B, rt_bypass_res_B, wm_bypass_res_B;
 
-    lc4_alu Pipeline_Alu ( 
-            .i_insn(x2m_bus[15:0]),
-            .i_pc(x2m_pc),
-            .i_r1data(rs_bypass_res),
-            .i_r2data(rt_bypass_res),
-            .o_result(o_alu_result)
+    lc4_alu PipeA_Alu ( 
+            .i_insn(x2m_bus_A[15:0]),
+            .i_pc(x2m_pc_A),
+            .i_r1data(rs_bypass_res_A),
+            .i_r2data(rt_bypass_res_A),
+            .o_result(o_alu_result_B)
     );
+
+    lc4_alu PipeB_Alu ( 
+            .i_insn(x2m_bus_B[15:0]),
+            .i_pc(x2m_pc_B),
+            .i_r1data(rs_bypass_res_B),
+            .i_r2data(rt_bypass_res_B),
+            .o_result(o_alu_result_B)
+    );
+
     
     // Bypass results assignment//
     assign rs_bypass_res =  ((x2m_bus[33:31] == m2w_bus[27:25]) && (m2w_bus[22] == 1)) ? m_O_o:         // MX for ALU input rs_data
