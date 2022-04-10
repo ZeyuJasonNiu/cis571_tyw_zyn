fc4e26071ef77ed8734b688c3abce1d32aab460b
Zeyu Jason Niu <jasonzy@huntsman-ve703-06865.apn.wlan.private.upenn.edu>
2022-04-10 17:06:21 -0400

    lab5-alu

--- lab5-superscalar/lc4_superscalar.v
+++ lab5-superscalar/lc4_superscalar.v
@@ -427,7 +427,7 @@ module lc4_processor(input wire         clk,             // main clock
     * to conditionally print out information.
     */
    always @(posedge gwe) begin
-       if ($time >= 1 && $time <=2000) begin
+       if ($time >= 1 && $time <=20000) begin
        $display("**************** New Cycle ***************");
        $display("Time = %d, stall_A = %h, stall_B = %h, o_cur_pc = %h, next_pc_A %h, i_cur_insn_A = %h, i_cur_insn_B = %h, test_cur_insn_A = %h, test_cur_insn_B = %h \n \n ", 
                 $time, test_stall_A, test_stall_B, o_cur_pc, next_pc_A, i_cur_insn_A, i_cur_insn_B, test_cur_insn_A, test_cur_insn_B);
