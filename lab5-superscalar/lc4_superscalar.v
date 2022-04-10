2861e6a2c70a6afa9313952bcfcb7a8751c127d6
Zeyu Jason Niu <jasonzy@huntsman-ve703-06865.apn.wlan.private.upenn.edu>
2022-04-09 23:42:55 -0400

    lab5-trial

--- lab5-superscalar/lc4_superscalar.v
+++ lab5-superscalar/lc4_superscalar.v
@@ -108,10 +108,9 @@ module lc4_processor(input wire         clk,             // main clock
     assign x_stall_i_A =  (LTU_A == 1) ? 2'd3 : 
                           d_stall_o_A;
 
-    assign x_stall_i_B = (x_br_taken_or_ctrl_A == 1 || x_br_taken_or_ctrl_B == 1) ? 2'd2 :
-                         (LTU_A == 1 || (B_need_A || mem_hazard)) ? 2'd1 :
-                         (LTU_B == 1) ? 2'd3 :
-                         d_stall_o_B;
+    assign x_stall_i_B =    (LTU_A == 1 || (B_need_A || mem_hazard)) ? 2'd1 :
+                            (LTU_B == 1) ? 2'd3 :
+                            d_stall_o_B;
 
     // assign x_stall_i_B = (LTU_A == 1) ? 2'd1 :
     //                      (B_need_A || mem_hazard) ? 2'd1 :
