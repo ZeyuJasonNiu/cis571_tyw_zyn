9e72322ce6bb71c93b41c951c405da8537051dd2
Zeyu Jason Niu <jasonzy@huntsman-ve703-07118.apn.wlan.private.upenn.edu>
2022-04-15 17:34:21 -0400

    lab5-all try

--- lab5-superscalar/lc4_superscalar.v
+++ lab5-superscalar/lc4_superscalar.v
@@ -348,10 +348,10 @@ module lc4_processor(input wire         clk,             // main clock
     assign test_dmem_addr_A = ((m2w_bus_A[19] == 1) || (m2w_bus_A[18] == 1)) ? m_O_o_A : 16'b0;
     assign test_dmem_addr_B = ((m2w_bus_B[19] == 1) || (m2w_bus_B[18] == 1)) ? m_O_o_B : 16'b0;
 
-    assign o_dmem_addr =    ((m2w_bus_A[19]) || (m2w_bus_A[18])) ? test_dmem_addr_A :
-                            ((m2w_bus_B[19]) || (m2w_bus_B[18])) ? test_dmem_addr_B : 
-                            16'b0000; 
-    // assign o_dmem_addr = test_dmem_addr_A | test_dmem_addr_B; 
+    // assign o_dmem_addr =    ((m2w_bus_A[19]) || (m2w_bus_A[18])) ? test_dmem_addr_A :
+    //                         ((m2w_bus_B[19]) || (m2w_bus_B[18])) ? test_dmem_addr_B : 
+    //                         16'b0; 
+    assign o_dmem_addr = test_dmem_addr_A | test_dmem_addr_B; 
     
     // assign i_cur_dmem_data_A = test_dmem_addr_A ? i_cur_dmem_data : 16'b0; 
     // assign i_cur_dmem_data_B = test_dmem_addr_B ? i_cur_dmem_data : 16'b0;    
