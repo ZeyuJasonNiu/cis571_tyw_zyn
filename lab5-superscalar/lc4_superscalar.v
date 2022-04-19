8c357ada61350969b9e78213b38c5273aa3555e1
Zeyu Jason Niu <jasonzy@client0253vag.vpn.private.upenn.edu>
2022-04-18 23:06:25 -0400

    lab5-all try

--- lab5-superscalar/lc4_superscalar.v
+++ lab5-superscalar/lc4_superscalar.v
@@ -476,7 +476,7 @@ module lc4_processor(input wire         clk,             // main clock
     * to conditionally print out information.
     */
    always @(posedge gwe) begin
-       if ($time >= 4800 && $time <=5200) begin
+       if ($time >= 4700 && $time <=5200) begin
         $display("=============================================");
         $display("Time = %d, o_cur_pc = %h, next_pc_A %h, i_cur_insn_A = %h, i_cur_insn_B = %h, test_cur_insn_A = %h, test_cur_insn_B = %h \n", 
                 $time, o_cur_pc, next_pc_A, i_cur_insn_A, i_cur_insn_B, test_cur_insn_A, test_cur_insn_B);
